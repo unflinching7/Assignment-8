e58f3ad034561819cdfa6e46e0244febcc1a44c3
unflinching7 <daburkett1@buffs.wtamu.edu>
2023-05-05 22:56:05 -0700

    pm2

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,6 +1,6 @@
 import pytest
 from allocation.adapters import repository
-from allocation.service_layer import services, unit_of_work
+from allocation.service_layer import handlers, unit_of_work
 
 
 class FakeRepository(repository.AbstractRepository):
@@ -28,36 +28,36 @@ class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
 
 def test_add_batch_for_new_product():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
+    handlers.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
     assert uow.products.get("CRUNCHY-ARMCHAIR") is not None
     assert uow.committed
 
 
 def test_add_batch_for_existing_product():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "GARISH-RUG", 100, None, uow)
-    services.add_batch("b2", "GARISH-RUG", 99, None, uow)
+    handlers.add_batch("b1", "GARISH-RUG", 100, None, uow)
+    handlers.add_batch("b2", "GARISH-RUG", 99, None, uow)
     assert "b2" in [
         b.reference for b in uow.products.get("GARISH-RUG").batches]
 
 
 def test_allocate_returns_allocation():
     uow = FakeUnitOfWork()
-    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
-    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
+    handlers.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
+    result = handlers.allocate("o1", "COMPLICATED-LAMP", 10, uow)
     assert result == "batch1"
 
 
 def test_allocate_errors_for_invalid_sku():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "AREALSKU", 100, None, uow)
+    handlers.add_batch("b1", "AREALSKU", 100, None, uow)
 
-    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate("o1", "NONEXISTENTSKU", 10, uow)
+    with pytest.raises(handlers.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        handlers.allocate("o1", "NONEXISTENTSKU", 10, uow)
 
 
 def test_allocate_commits():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
-    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+    handlers.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
+    handlers.allocate("o1", "OMINOUS-MIRROR", 10, uow)
     assert uow.committed
